6e4352d089981851df916ebacc578d5e412f72a9
tsremarkable <tsremarkable@gmail.com>
2023-02-18 21:53:54 -0500

    add: contact

--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -3,8 +3,9 @@ import Navbar from "./Navbar";
 const RegistrationPage = () => {
     return(
         <div className="">
-            <div className="bg-[#4D6B4A] flex justify-center items-center">
-                <h1 className="text-3xl p-10 text-white">Bro Please message me to <span className="text-black text-4xl">tsremarkable@gmail.com</span> <span className="text-6xl text-[#8BAF89]">you</span></h1>
+            <div className="bg-[#4D6B4A] flex justify-center items-center flex-col">
+                <h1 className="text-3xl p-10 text-white">Bro Please message me to <span className="text-black text-4xl">tsremarkable@gmail.com</span></h1>
+                <h1 className="text-3xl p-10 text-white">this is my TG <span className="text-black text-4xl">https://t.me/hereweb</span></h1>
             </div>
             <Navbar />
         </div>
