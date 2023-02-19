d3c33f7a2034354767098caab609646d04ec03d8
tsremarkable <tsremarkable@gmail.com>
2023-02-18 21:44:59 -0500

    add: contact

--- src/pages/RegistrationPage/index.tsx
+++ src/pages/RegistrationPage/index.tsx
@@ -4,7 +4,7 @@ const RegistrationPage = () => {
     return(
         <div className="">
             <div className="bg-[#4D6B4A] flex justify-center items-center">
-                <h1 className="text-3xl p-10 text-white">Bro Please message me to tsremarkable@gmail.com <span className="text-6xl text-[#8BAF89]">you</span></h1>
+                <h1 className="text-3xl p-10 text-white">Bro Please message me to <span className="text-black text-4xl">tsremarkable@gmail.com</span> <span className="text-6xl text-[#8BAF89]">you</span></h1>
             </div>
             <Navbar />
         </div>
